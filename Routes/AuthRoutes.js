7eb82cfdf4a26e4315516a61a4f5101ca00baca2
Leo <82315240+leohonlmc@users.noreply.github.com>
2023-09-23 20:10:12 -0400

    web host site for frontend added

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -6,7 +6,7 @@ const router = require("express").Router();
 router.post("/google/register", googleRegister);
 router.get("/user/:id", getUser);
 
-router.post("/upload", uploadImage);
+router.post("/upload/image", uploadImage);
 
 router.get("/allImages/:sub", getAllImage);
 
